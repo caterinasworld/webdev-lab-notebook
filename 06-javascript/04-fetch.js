746c9ddf8a529ed17e21ebb836e79fddd99bd6e2
patkra <patkra@pdx.edu>
2026-05-24 20:07:02 -0700

    just needs searchbar

--- 06-javascript/04-fetch.js
+++ 06-javascript/04-fetch.js
@@ -5,16 +5,16 @@ const pokemonColors = {
   electric: "#F7D02C",
   grass: "#7AC74C",
   ice: "#96D9D6",
-  fighting: "#C22E28",
+  fighting: "#c27370",
   poison: "#ea7ce8",
   ground: "#E2BF65",
   flying: "#A98FF3",
   psychic: "#F95587",
   bug: "#A6B91A",
   rock: "#B6A136",
-  ghost: "#735797",
-  dragon: "#6F35FC",
-  dark: "#705746",
+  ghost: "#867f91",
+  dragon: "#906fe4",
+  dark: "#929292",
   steel: "#B7B7CE",
   fairy: "#D685AD",
 };
@@ -30,7 +30,7 @@ const createNewElement = function (data) {
   h2.textContent = data.name;
 
   img.src = data.sprites.other["official-artwork"].front_default;
-  img.alt = `image of ${data.name}`;
+  img.alt = `the pokemon ${data.name}`;
   img.width = "240";
   img.height = "240";
 
@@ -76,4 +76,48 @@ const fetchData = async function () {
   }
 };
 
-fetchData();
+// fetchData();
+
+const fetchAll = async function () {
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=251&offset=0";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const pokemonList = data.results;
+
+    console.log(data.results);
+
+    const promises = pokemonList.map((pokemon) =>
+      fetch(pokemon.url).then((res) => res.json()),
+    );
+
+    const pokemonData = await Promise.all(promises);
+
+    console.log(pokemonData);
+
+    pokemonData.forEach((pokemon) => {
+      const elem = createNewElement(pokemon);
+      const pokeList = document.querySelector(".poke-list");
+      pokeList.appendChild(elem);
+    });
+
+    // const elem = createNewElement(bodyData);
+    // const pokeList = document.querySelector(".poke-list");
+    // pokeList.appendChild(elem);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    const errorElem = document.createElement("p");
+    errorElem.textContent = "Failed to fetch data. Please try again later.";
+    errorElem.setAttribute("class", "error-message");
+    const pokeList = document.querySelector(".poke-list");
+    pokeList.appendChild(errorElem);
+  } finally {
+    console.log("Fetch attempt completed.");
+    const loading = document.querySelector(".loading-container");
+    loading.setAttribute("class", "display-none");
+  }
+};
+
+fetchAll();
